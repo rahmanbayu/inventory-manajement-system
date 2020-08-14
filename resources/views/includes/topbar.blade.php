172a276fb429a232e2a593bc8d164f376a94f87c
rahmanbayu <rahmanbayu4@gmail.com>
2020-08-14 12:41:15 +0700

    add image in users table

--- resources/views/includes/topbar.blade.php
+++ resources/views/includes/topbar.blade.php
@@ -119,8 +119,12 @@
                 <!-- Nav Item - User Information -->
                 <li class="nav-item dropdown no-arrow">
                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">Valerie Luna</span>
-                    <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
+                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ ucwords(Auth::user()->name) }}</span>
+                    @if (!Auth::user()->image)
+                    <img class="img-profile rounded-circle" src="{{ asset('img/default/def.png') }}">
+                    @else
+                    <img class="img-profile rounded-circle" src="{{ Storage::url(Auth::user()->image) }}">
+                    @endif
                 </a>
                 <!-- Dropdown - User Information -->
                 <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
